687e22c3f40553a27a68a8e7d043b7c273253cbc
Sara Rocha de Oliveira <sararocha878@gmail.com>
2023-04-19 13:03:50 -0300

    feat handleDeleteFile

--- client/src/pages/Folder/index.jsx
+++ client/src/pages/Folder/index.jsx
@@ -1,29 +1,78 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 
 import { FiSearch } from "react-icons/fi";
 import { RiDeleteBin6Line, RiDownloadCloud2Line } from "react-icons/ri";
 
 import MainLayout from "../../layouts/MainLayout/MainLayout";
+import Input from "../../components/Input";
+
 import { getOneFolder } from "../../services/foldersApi";
+import { deleteFile, listFiles } from "../../services/filesApi";
+import { UserContext } from "../../context/userContext";
 
 import imgNuvem from "../../images/png/upload-na-nuvem.png"
 
 import "./style.css";
-import Input from "../../components/Input";
+import { debounce } from "lodash";
+import { toast } from "react-toastify";
 
 const Folder = () => {
   const { id } = useParams();
+  const { userData } = useContext(UserContext);
   const [folder, setFolder] = useState([]);
+  const [files, setFiles] = useState([]);
+  const [valueInputSearchDoc, setValueInputSearchDoc] = useState("");
+  const [filteredFiles, setFilteredFiles] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchDataFolder = async () => {
       const data = await getOneFolder(id);
       setFolder(data)
     }
 
-    fetchData();
+    fetchDataFiles();
+    fetchDataFolder();
   }, []);
+
+
+  const fetchDataFiles = async () => {
+    const data = await listFiles(id);
+    setFiles(data)
+  }
+
+  const searchFilesDoc = useCallback(
+    debounce((value, files) => filterFiles(value, files), 500),
+    []
+  );
+
+  const filterFiles = (value, files) => {
+    const newArray = files.filter((i) => i.original_name.toUpperCase().trim() === value.toUpperCase().trim());
+    setFilteredFiles(newArray);
+  }
+
+  const handleFormatDate = (date) => {
+    const newDate = new Date(date);
+    const dataformatted = newDate.toLocaleString('pt-BR', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit'
+    }).replace(',', ' -');
+
+    return dataformatted;
+  }
+
+  const handleDeleteFile = async (item) => {
+    try {
+      await deleteFile(item.id);
+      fetchDataFiles();
+      toast.success("Excluido");
+    } catch (error) {
+      toast.error("Erro ao excluir");
+    }
+  }
 
   return (
     <MainLayout pageTitle={folder?.title} btnReturn={true}>
@@ -45,16 +94,15 @@ const Folder = () => {
             placeholder="Pesquisar documento"
             padding="5px 45px"
             marginTopLabel="0"
-          // value={valueInputSearchFolder}
-          // onChange={(value) => {
-          //   setValueInputSearchFolder(value);
-          //   searchFolders(value, folders);
-          // }}
+            value={valueInputSearchDoc}
+            onChange={(value) => {
+              setValueInputSearchDoc(value);
+              searchFilesDoc(value, files);
+            }}
           />
 
           <FiSearch className="svgSearchDoc" />
         </span>
-
 
         <table className="containerTable">
           <tr className="containerTr">
@@ -66,14 +114,43 @@ const Folder = () => {
             <th></th>
           </tr>
 
-          <tr className="containerTr containerTrWidth textPoppinsCheckBox">
-            <td className="containerTh">Alfreds Futterkiste</td>
-            <td className="containerTh">Maria Anders</td>
-            <td className="containerTh">Germany</td>
-            <td className="containerTh">aa</td>
-            <td className="containerTh"> <RiDownloadCloud2Line className="svgFile"/> <RiDeleteBin6Line className="svgFile"/> </td>
-          </tr>
-  
+          {filteredFiles.length > 0 ?
+            filteredFiles.map((item) => {
+              const dateformatted = handleFormatDate(item.updatedAt)
+              return (
+                <tr className="containerTr containerTrWidth textPoppinsCheckBox" key={item.id}>
+                  <td className="containerTh">{item.original_name}</td>
+                  <td className="containerTh">{userData?.name}</td>
+                  <td className="containerTh">{dateformatted}</td>
+                  <td className="containerTh">{item.size}</td>
+                  <td className="containerTh" >
+                    <RiDownloadCloud2Line className="svgFile" />
+                    <RiDeleteBin6Line className="svgFile" onClick={() => handleDeleteFile(item)} />
+                  </td>
+                </tr>
+              )
+            })
+
+            :
+            Boolean(files.length) && files.map((item) => {
+              const dateformatted = handleFormatDate(item.updatedAt)
+
+              return (
+                <tr className="containerTr containerTrWidth textPoppinsCheckBox" key={item.id}>
+                  <td className="containerTh">{item.original_name}</td>
+                  <td className="containerTh">{userData?.name}</td>
+                  <td className="containerTh">{dateformatted}</td>
+                  <td className="containerTh">{item.size}</td>
+                  <td className="containerTh">
+                    <RiDownloadCloud2Line className="svgFile" />
+                    <RiDeleteBin6Line className="svgFile" onClick={() => handleDeleteFile(item)} />
+                  </td>
+                </tr>
+              )
+            })
+          }
+
+
         </table>
       </div>
     </MainLayout>
